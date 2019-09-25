f5816af086351070e3e83367e934171afab1d949
hgf-anu <956246882@qq.com>
2019-09-25 15:58:33 +0800

    updata_01

--- src/main/scala/com/yaxin/release/etl/dw/DWReleaseColumnsHelper.scala
+++ src/main/scala/com/yaxin/release/etl/dw/DWReleaseColumnsHelper.scala
@@ -1,5 +1,34 @@
 package com.yaxin.release.etl.dw
 
-object DWReleaseColumnsHelper{
+import scala.collection.mutable.ArrayBuffer
 
+/**
+  * DW层 投放业务列表
+  */
+object DWReleaseColumnsHelper{
+	/**
+    * 目标用户
+    */
+  def selectDWReleaseCustomerColumns():ArrayBuffer[String]={
+    val columns = new ArrayBuffer[String]()
+    columns.+=("release_session")
+    columns.+=("release_status")
+    columns.+=("device_num")
+    columns.+=("device_type")
+    columns.+=("sources")
+    columns.+=("channels")
+    columns.+=("get_json_object(exts,'$.idcard') as idcard")
+    columns.+=("(cast(date_format(now(),'yyyy')as int) - cast(regexp_extract(get_json_object(exts,'$.idcard'),'(\\\\d{6})(\\\\d{4})',2) as int))as age")
+    columns.+=("cast(regexp_extract(get_json_object(exts,'$.idcard'),'(\\\\d{16})(\\\\d{1})()',2) as int) % 2 as gender")
+    columns.+=("get_json_object(exts,'$.area_code') as area_code")
+    columns.+=("get_json_object(exts,'$.longitude') as longitude")
+    columns.+=("get_json_object(exts,'$.latitude') as latitude")
+    columns.+=("get_json_object(exts,'$.matter_id') as matter_id")
+    columns.+=("get_json_object(exts,'$.model_code') as model_code")
+    columns.+=("get_json_object(exts,'$.model_version') as model_version")
+    columns.+=("get_json_object(exts,'$.aid') as aid")
+    columns.+=("ct")
+    columns.+=("bdp_day")
+    columns
+  }
 }
--- src/main/scala/com/yaxin/release/etl/dw/DWReleaseCustomer.scala
+++ src/main/scala/com/yaxin/release/etl/dw/DWReleaseCustomer.scala
@@ -3,6 +3,14 @@ package com.yaxin.release.etl.dw
 class DWReleaseCustomer{
 
 }
-object DWReleaseCustomer{
 
+/**
+  * 目标客户
+  *  release_status=01
+  */
+object DWReleaseCustomer{
+	def main(args:Array[String]):Unit ={
+		val appName = "dw_release_job"
+		val bdp_day_begin = "20190613"
+	}
 }
